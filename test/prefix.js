ba04510dc7a97e10f6b1128215ff7fac94f0ba99
TJ Holowaychuk <tj@vision-media.ca>
2013-06-23 16:36:00 -0700

    add support for mapping function

--- test/prefix.js
+++ test/prefix.js
@@ -15,3 +15,18 @@ describe('props(str, prefix)', function(){
     .should.equal('$.first() + $.last() + $.classes.join()');
   })
 })
+
+describe('props(str, fn)', function(){
+  it('should map properties', function(){
+    props('foo + bar', function(_){ return '$.' + _ })
+    .should.equal('$.foo + $.bar');
+
+    props('[foo, bar, baz].join("foo bar baz")', function(_){ return '$.' + _ })
+    .should.equal('[$.foo, $.bar, $.baz].join("foo bar baz")');
+  })
+
+  it('should map methods', function(){
+    props('first() + last() + classes.join()', function(_){ return '$.' + _ })
+    .should.equal('$.first() + $.last() + $.classes.join()');
+  })
+})
