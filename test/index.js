29471166aa8a446407f4c1080a659e6ba866556e
Recursive Lambda God <chris@circle.academy>
2017-04-29 14:15:39 -0400

    "Recursive Lambda Refactor V1"

--- test/index.js
+++ test/index.js
@@ -3,11 +3,11 @@ var props = require('..');
 var fs = require('fs');
 var path = require('path');
 
-fs.readdirSync('test/cases').forEach(function(file){
+fs.readdirSync('test/cases').forEach(file => {
   if (~file.indexOf('.out')) return;
   var base = path.basename(file, '.js');
-  describe(base, function(){
-    it('should work', function(){
+  describe(base, () => {
+    it('should work', () => {
       var js = fs.readFileSync('test/cases/' + file, 'utf8');
       var out = fs.readFileSync('test/cases/' + base + '.out', 'utf8').trim().split(/\s+/);
       props(js).should.eql(out);
--- test/prefix.js
+++ test/prefix.js
@@ -1,8 +1,8 @@
 
 var props = require('..');
 
-describe('props(str, prefix)', function(){
-  it('should prefix properties', function(){
+describe('props(str, prefix)', () => {
+  it('should prefix properties', () => {
     props('foo + bar', '$.')
     .should.equal('$.foo + $.bar');
 
@@ -10,28 +10,28 @@ describe('props(str, prefix)', function(){
     .should.equal('[$.foo, $.bar, $.baz].join("foo bar baz")');
   })
 
-  it('should prefix methods', function(){
+  it('should prefix methods', () => {
     props('first() + last() + classes.join()', '$.')
     .should.equal('$.first() + $.last() + $.classes.join()');
   })
 
-  it('should ignore global constructs', function(){
+  it('should ignore global constructs', () => {
     props('Math.round(n) * JSON.stringify(blob)', '$.')
     .should.equal('Math.round($.n) * JSON.stringify($.blob)');
   })
 })
 
-describe('props(str, fn)', function(){
-  it('should map properties', function(){
-    props('foo + bar', function(_){ return '$.' + _ })
+describe('props(str, fn)', () => {
+  it('should map properties', () => {
+    props('foo + bar', _ => '$.' + _)
     .should.equal('$.foo + $.bar');
 
-    props('[foo, bar, baz].join("foo bar baz")', function(_){ return '$.' + _ })
+    props('[foo, bar, baz].join("foo bar baz")', _ => '$.' + _)
     .should.equal('[$.foo, $.bar, $.baz].join("foo bar baz")');
   })
 
-  it('should map methods', function(){
-    props('first() + last() + classes.join()', function(_){ return '$.' + _ })
+  it('should map methods', () => {
+    props('first() + last() + classes.join()', _ => '$.' + _)
     .should.equal('$.first() + $.last() + $.classes.join()');
   })
 })
